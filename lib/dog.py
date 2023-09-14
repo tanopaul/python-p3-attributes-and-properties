2b0aa2df489375e86d8311aa95d0d28a8d30d251
Tano Paul <tanopaul38@gmail.com>
2023-09-14 00:05:48 -0400

    completed attributes and properties lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,39 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name='Tano', breed='Mastiff'):
+        self.name = name
+        self.breed = breed
+
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print('Breed must be in list of approved breeds.')
+
+    breed = property(get_breed, set_breed)
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if type(name) == str and 0 < len(name) <= 25:
+            self._name = name.capitalize()
+            return self.name
+        else:
+            print('Name must be string between 1 and 25 characters.')
+
+    name = property(get_name, set_name)
+
+   
+
+
+fido = Dog()
+fido.name = ''
+fido.breed = 'Corgi'
+
+print(fido.name)
+print(fido.breed)
